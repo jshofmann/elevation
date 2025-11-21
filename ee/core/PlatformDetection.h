5fec78c5c06aae566e60600eee3b0f1aeb298f87
Scott Hofmann <shofmann@adobe.com>
2025-11-21 09:33:58 -0700

    - MSVC should set /Zc:__cplusplus so that the __cplusplus macro value matches that of other compilers and MSVC's /std:c++## command-line argument (see https://learn.microsoft.com/en-us/cpp/build/reference/zc-cplusplus) - Separated CPU architecture detection from OS detection - Tweaks to .clang-format

--- ee/core/PlatformDetection.h
+++ ee/core/PlatformDetection.h
@@ -9,7 +9,7 @@
 #endif
 
 ///////////////////////////////////////////////////////////////////////////////
-// OS and compiler detection
+// Compiler detection
 
 // Microsoft Visual Studio is being used,
 // but don't define EE_COMPILER_MSVC if the clang-cl toolset is being used
@@ -26,55 +26,59 @@
 #  error Unsupported compiler
 #endif
 
+///////////////////////////////////////////////////////////////////////////////
+// CPU architecture detection
+
+#if defined(__i386__) || defined(_M_IX86)
+#  define EE_BUILD_X86			1
+#elif defined( __x86_64__ ) || defined( _M_X64 ) || defined( __amd64__ ) || defined( _M_AMD64 )
+#  define EE_BUILD_X64			1
+#elif defined( __arm__ ) || defined( _M_ARM )
+#  define EE_BUILD_ARM			1
+#  define EE_BUILD_ARM32		1
+#elif defined( __aarch64__ ) || defined( _M_ARM64 )
+#  define EE_BUILD_ARM			1
+#  define EE_BUILD_ARM64		1
+#elif defined( __wasm32__ )
+#  define EE_BUILD_WASM			1
+#  define EE_BUILD_WASM32		1
+#elif defined( __wasm64__ )
+#  define EE_BUILD_WASM			1
+#  define EE_BUILD_WASM64		1
+#else
+#  error Unsupported CPU architecture
+#endif
+
+///////////////////////////////////////////////////////////////////////////////
+// OS detection
+
 // Scarlett and Durango also define WIN32 and WIN64 so test for them first
 #if defined( _GAMING_XBOX_SCARLETT )	// Xbox Series X (Anaconda) or Xbox Series S (Lockhart)
 #  define EE_BUILD_XSX				1
-#  define EE_BUILD_X64				1
-#  define EE_BUILD_LITTLE_ENDIAN	1
 #elif defined( _XBOX_ONE )				// aka Durango
 #  define EE_BUILD_XB1				1
-#  define EE_BUILD_X64				1
-#  define EE_BUILD_LITTLE_ENDIAN	1
 #elif defined( _WIN32 ) || defined( _WIN64 ) // Windows PC
 // Note: BUILD_WINDOWS is used by some Windows SDK headers so avoid that name
 #  define EE_BUILD_WINDOWS			1
-#  define EE_BUILD_LITTLE_ENDIAN	1
-#  if defined( EE_COMPILER_MSVC )
-#    if defined( _AMD64_ ) || defined( _M_X64 ) || defined( _M_AMD64 )
-#      define EE_BUILD_X64			1
-#    elif defined( _M_ARM )
-#      define EE_BUILD_ARM			1
-#      define EE_BUILD_ARM32		1
-#    elif defined( _M_ARM64 )
-#      define EE_BUILD_ARM			1
-#      define EE_BUILD_ARM64		1
-#    endif
-#  elif defined( EE_COMPILER_GCC )
-#    if defined( _X86_64_ ) || defined( __amd64__ )
-#      define EE_BUILD_X64			1
-#    endif
-#  endif
 #elif defined( __ORBIS__ )		// Sony Playstation 4
 #  define EE_BUILD_PS4				1
-#  define EE_BUILD_X64				1
-#  define EE_BUILD_LITTLE_ENDIAN	1
+#  define EE_BUILD_POSIX			1
 #elif defined( __PROSPERO__ )	// Sony Playstation 5
 #  define EE_BUILD_PS5				1
-#  define EE_BUILD_X64				1
-#  define EE_BUILD_LITTLE_ENDIAN	1
+#  define EE_BUILD_POSIX			1
 #elif defined( __NX__ )			// Nintendo Switch
 #  define EE_BUILD_NX				1
-#  define EE_BUILD_ARM				1
-#  if defined( __ARM_32BIT_STATE )
-#    define EE_BUILD_ARM32			1
-#  endif
-#  if defined( __ARM_64BIT_STATE )
-#    define EE_BUILD_ARM64			1
-#  endif
-#  define EE_BUILD_LITTLE_ENDIAN	1
 #elif defined( __APPLE__ )
 #  define EE_BUILD_APPLE			1
-#  define EE_BUILD_LITTLE_ENDIAN	1
+#  define EE_BUILD_POSIX			1
+#  include <TargetConditionals.h>
+#  if TARGET_OS_IPHONE			// TARGET_OS_IPHONE also defines TARGET_OS_MAC
+#    define EE_BUILD_IOS			1
+#  elif TARGET_OS_MAC
+#    define EE_BUILD_MACOS			1
+#  else
+#    error Unsupported Apple platform
+#  endif
 #  if TARGET_CPU_X86_64
 #    define EE_BUILD_X64			1
 #  elif TARGET_CPU_ARM64
@@ -83,31 +87,23 @@
 #  endif
 #elif defined( __EMSCRIPTEN__ )
 #  define EE_BUILD_EMSCRIPTEN		1
+#  define EE_BUILD_POSIX			1
 #elif defined( __ANDROID__ )	// Android also defines __linux__
 #  define EE_BUILD_ANDROID			1
-#  if defined( __aarch64__ )
-#    define EE_BUILD_ARM			1
-#    define EE_BUILD_ARM64			1
-#  elif defined( _M_ARM )
-#    define EE_BUILD_ARM			1
-#    define EE_BUILD_ARM32			1
-#  elif defined( __x86_64__ )
-#    define EE_BUILD_X64			1
-#  endif
+#  define EE_BUILD_POSIX			1
 #elif defined( __linux__ )      // Includes Stadia and SteamOS
 #  define EE_BUILD_LINUX			1
-#  if defined( _X86_64_ ) || defined( __amd64__ )
-#    define EE_BUILD_X64			1
-#  endif
-#  define EE_BUILD_LITTLE_ENDIAN	1
+#  define EE_BUILD_POSIX			1
 #else
-#  error Unknown platform
+#  error Unsupported platform
 #endif
 
-#if defined( EE_BUILD_X64 ) || defined( EE_BUILD_ARM64 )
+#if defined( EE_BUILD_X64 ) || defined( EE_BUILD_ARM64 ) || defined( EE_BUILD_WASM64 )
 #  define EE_BUILD_64BIT 1
+static_assert( sizeof( sizeof( char ) ) == 8, "Expected sizeof( size_t ) == 8" );
 #else
 #  define EE_BUILD_32BIT 1
+static_assert( sizeof( sizeof( char ) ) == 4, "Expected sizeof( size_t ) == 4" );
 #endif
 
 #if defined( _DEBUG ) || defined( EE_BUILD_DEVELOP )
